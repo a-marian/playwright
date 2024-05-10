56222d8605711a78226354689f72405125ca66a4
marelas <marela.as@gmail.com>
2024-05-09 23:50:50 -0300

    initial commit

--- src/test/java/TestContext.java
+++ src/test/java/TestContext.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class TestContext {
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Tag("playwright")
+public class TestContext {
+
+    // This will be injected with the random free port
+    // number that was allocated
+    @LocalServerPort
+    private int port;
+
+    static Playwright playwright = Playwright.create();
+
+    @Test
+    public void testClicking() {
+
+            Browser browser = playwright.chromium().launch(
+                   new BrowserType.LaunchOptions().setHeadless(false)
+            );
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+            // create a locator
+            Locator getStarted = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Get Started"));
+            // Expect an attribute "to be strictly equal" to the value.
+            assertThat(getStarted).hasAttribute("href", "/docs/intro");
+
+            // Click the get started link.
+            getStarted.click();
+
+            // Expects page to have a heading with the name of Installation.
+            assertThat(page.getByRole(AriaRole.HEADING,
+                    new Page.GetByRoleOptions().setName("Installation"))).isVisible();
+
+    }
 }
